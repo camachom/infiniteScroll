969b80622f82b16456663b2d897a3d3836d1dcac
camachom <mcama200@gmail.com>
2017-03-01 15:08:54 -0800

    naive infinite scroll working

--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,7 @@
 // @flow
 
 import React, { PropTypes } from 'react';
-import { PinterestPinWidget } from 'react-pinterest';
-// import Pin from './Pin.jsx';
+import Pin from './Pin.jsx';
 import _ from 'lodash';
 import './List.css';
 
@@ -15,23 +14,11 @@ class List extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {start_idx: 0, end_idx: 10, pins: [], cache: {}};
+    this.state = {start_idx: 0, end_idx: 10, pins: this.props.pins.slice(0,10)};
 
-    (this:any).handleScrollCalculations = _.throttle(this.handleScrollCalculations.bind(this), 250);
-    // (this:any).widget = this.widget.bind(this);
+    (this:any).handleScrollCalculations = _.throttle(this.handleScrollCalculations.bind(this), 200);
     (this:any).pinsToRender = this.pinsToRender.bind(this);
     (this:any).handleScrollEvent = this.handleScrollEvent.bind(this);
-  }
-
-  componentWillMount() {
-    const cache = {};
-    const pinIds = this.props.pins.slice(0,10).map( pin => {
-      if(!cache[pin.id]) {
-        cache[pin.id] = <PinterestPinWidget key={pin.id} size="small" pin={pin.id}/>
-      }
-      return pin.id;
-    });
-    this.setState({pins: pinIds, cache: cache});
   }
 
   handleScrollCalculations(e) {
@@ -39,23 +26,18 @@ class List extends React.Component {
     const scrTop = e.target.scrollTop;
     const clientHeight = e.target.clientHeight;
 
-    if (clientHeight + scrTop + 100 >= scrHeight) {
+    if (clientHeight + scrTop + 200 >= scrHeight) {
       const end_idx = this.state.end_idx;
 
       const newStartIdx = end_idx % 50;
-      const newEndIdx = (newStartIdx + 10) % 50;
+      let newEndIdx = (newStartIdx + 10) % 50;
+      // avoid getting an invalid range of (40..0)
+      newEndIdx === 0 ? newEndIdx = 50 : null;
+
       const pins = this.pinsToRender(newStartIdx, newEndIdx);
-
-      // caching the previously rendered components
       debugger;
-      const pinIds = pins.map( (pinId, idx) => {
-        if(!this.state.cache[pinId]) {
-          this.state.cache[pinId] = <PinterestPinWidget key={pinId} size="small" pin={pinId}/>
-        }
-        return pinId;
-      });
 
-      this.setState({start_idx: newStartIdx, end_idx: newEndIdx, pins: pinIds, cache: this.state.cache});
+      this.setState({start_idx: newStartIdx, end_idx: newEndIdx, pins: pins});
     }
   }
 
@@ -64,51 +46,10 @@ class List extends React.Component {
     this.handleScrollCalculations(e);
   }
 
-  // componentDidMount(nextProps: Object, nextState: Object) {
-  //   this.widget();
-  // }
-  //
-  // componentWillUpdate(){
-  //   const remove = this.widget();
-  //   if (remove) {
-  //     remove.remove();
-  //   }
-  // }
-
-  // widget() {
-  //   let firstScript, newScript, hazPinIt;
-  //
-  //   // generate an unique-ish global ID: hazPinIt_ plus today's Unix day
-  //   hazPinIt = 'PIN_' + ~~(new Date().getTime() / 86400000);
-  //
-  //   if (!window[hazPinIt]) {
-  //
-  //     // don't run next time
-  //     // w[hazPinIt] = true;
-  //     if (this.state.end_idx >= 50) {
-  //       window[hazPinIt] = true;
-  //     }
-  //
-  //     // avoid KB927917 error in IE8
-  //     // window.setTimeout(function () {
-  //       // load the bulk of pinit.js
-  //       firstScript = document.getElementsByTagName('SCRIPT')[0];
-  //       newScript = document.createElement('SCRIPT');
-  //       newScript.type = 'text/javascript';
-  //       newScript.async = true;
-  //       newScript.src = '//assets.pinterest.com/js/pinit_main.js';
-  //       firstScript.parentNode.insertBefore(newScript, firstScript);
-  //     // }, 10);
-  //       return newScript;
-  //   }
-  // }
-
   pinsToRender(startIdx, endIdx) {
-    const newPinIds = this.props.pins.slice(startIdx, endIdx).map( pin =>{
-      return pin.id;
-    })
+    const newPins = this.props.pins.slice(startIdx, endIdx)
 
-    const pinsToRender = this.state.pins.concat(newPinIds);
+    const pinsToRender = this.state.pins.concat(newPins);
     return pinsToRender;
   }
 
@@ -116,8 +57,8 @@ class List extends React.Component {
 
     return (
       <div onScroll={this.handleScrollEvent} className="List">
-        {this.state.pins.map( pin => {
-          return this.state.cache[pin];
+        {this.state.pins.map( (pin, idx) => {
+          return <Pin key={pin.id * idx} pin={pin} />;
         })}
       </div>
     )
@@ -129,9 +70,3 @@ List.propTypes = {
 };
 
 export default List;
-
-// {this.state.pins.map( (pin, idx) => {
-// return <PinterestPinWidget size="small" pin={pin.id}/>
-// return <Pin key={pin.id + Date.now() * idx} pin={pin} />;
-// })}
-// <div dangerouslySetInnerHTML={{__html: `<script async defer src="//assets.pinterest.com/js/pinit.js"></script>"`}}/>
--- src/Pin.jsx
+++ src/Pin.jsx
@@ -1,27 +1,33 @@
-// // @flow
-//
-// import React, { PropTypes } from 'react';
-//
-//
-// type Props = {
-//   pin: Object
-// }
-//
-// class Pin extends React.Component {
-//   props: Props;
-//
-//   render () {
-//     const id = this.props.pin.id;
-//     return(
-//       <PinterestPinWidget pin={id} />
-//       // <a data-pin-do="embedPin" href={`https://www.pinterest.com/pin/${id}/`}></a>
-//       // <div>{this.props.pin.title ? this.props.pin.title : id}</div>
-//     )
-//   }
-// }
-//
-// Pin.propTypes = {
-//   pin: PropTypes.object.isRequired
-// };
-//
-// export default Pin;
+// @flow
+
+import React, { PropTypes } from 'react';
+
+
+type Props = {
+  pin: Object
+}
+
+class Pin extends React.Component {
+  props: Props;
+
+  render () {
+    const pin = this.props.pin;
+    const imageURL = pin.images["236x"]["url"];
+    const title = pin.title;
+    const description = pin.description;
+
+    return(
+      <div>
+        <h3>{title}</h3>
+        <img src={imageURL} alt="Pin Component"/>
+        <div>{description}</div>
+      </div>
+    )
+  }
+}
+
+Pin.propTypes = {
+  pin: PropTypes.object.isRequired
+};
+
+export default Pin;
