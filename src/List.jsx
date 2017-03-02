0fa4f2c567d8d3efc85e5f088265552762b4f41c
camachom <mcama200@gmail.com>
2017-03-02 13:32:08 -0800

    empty div appraoch with bugs

--- src/List.jsx
+++ src/List.jsx
@@ -1,4 +1,5 @@
 // @flow
+// @flow
 
 import React, { PropTypes } from 'react';
 import Pin from './Pin.jsx';
@@ -14,36 +15,41 @@ class List extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {start_idx: 0, end_idx: 10, pins: this.props.pins.slice(0,10), rounds: 0, maxRounds: 0};
+    // constants
+    this.totalPins = this.props.pins.length;
+    this.pinsPerPage = 10;
+    this.elementHeight = 300;
+
+    this.state = {start_idx: 0, end_idx: this.pinsPerPage, pins: this.props.pins.slice(0, this.pinsPerPage), rounds: 0, maxRounds: 0};
 
     (this:any).handleScrollCalculations = _.throttle(this.handleScrollCalculations.bind(this), 200);
     (this:any).pinsToRender = this.pinsToRender.bind(this);
     (this:any).handleScrollEvent = this.handleScrollEvent.bind(this);
-    (this:any).calculatePinIdx = this.calculatePinIdx.bind(this);
+    (this:any).upScrollCalc = this.upScrollCalc.bind(this);
     (this:any).downScrollCalc = this.downScrollCalc.bind(this);
+    (this:any).downScrollIdxs = this.downScrollIdxs.bind(this);
+    (this:any).upScrollIdxs = this.upScrollIdxs.bind(this);
   }
 
   // change name to suit function
-  calculatePinIdx(reOrderFlag) {
-    const end_idx = this.state.end_idx;
-
-    const newStartIdx = end_idx % 50;
-    let newEndIdx = (newStartIdx + 10) % 50;
-    // avoid getting an invalid range of (40..0)
-    newEndIdx === 0 ? newEndIdx = 50 : null;
-    // make this more readable
+  upScrollCalc(reOrderFlag) {
+    const idxs = this.upScrollIdxs();
+    const newStartIdx = idxs[0];
+    const newEndIdx = idxs[1];
 
     let pins, rounds;
+    // reOrder means the whole data set has been rendered at least once
     if (reOrderFlag) {
       // keep state immutable by creating a copy
       const oldPins = this.props.pins.slice();
       // add ten empty divs here
-      const emptyDivs = Array(10 * this.state.rounds).fill("empty div");
+      const emptyDivs = Array(this.pinsPerPage * this.state.rounds).fill("empty div");
       const postEmpty = oldPins.slice(newEndIdx);
       const pastZeroPins = oldPins.slice(0, newEndIdx);
 
       pins = emptyDivs.concat(postEmpty.concat(pastZeroPins));
       rounds = this.state.rounds + 1;
+      console.log(pins);
     }
     else {
       pins = this.pinsToRender(newStartIdx, newEndIdx);
@@ -53,37 +59,63 @@ class List extends React.Component {
     this.setState({start_idx: newStartIdx, end_idx: newEndIdx, pins: pins, rounds: rounds, maxRounds: rounds > this.state.maxRounds ? rounds : this.state.rounds});
   }
 
-  downScrollCalc() {
-    const start_idx = this.state.start_idx;
+  upScrollIdxs() {
+    // NOTE: make this more readable
+    const end_idx = this.state.end_idx;
 
-    let newStartIdx = this.state.start_idx - 10;
-    start_idx === 0 ? newStartIdx = 40 : newStartIdx = start_idx - 10;
-    let newEndIdx = (newStartIdx + 10) % 50;
+    const newStartIdx = end_idx % this.totalPins;
+    let newEndIdx = (newStartIdx + this.pinsPerPage) % this.totalPins;
     // avoid getting an invalid range of (40..0)
-    newEndIdx === 0 ? newEndIdx = 50 : null;
-    // make this more readable
+    newEndIdx === 0 ? newEndIdx = this.totalPins : null;
+
+    return [newStartIdx, newEndIdx];
+  }
+
+  downScrollCalc() {
+    // try to write this better
+    const idxs = this.downScrollIdxs();
+    const newStartIdx = idxs[0];
+    const newEndIdx = idxs[1];
 
     const newRounds = this.state.rounds - 1;
     // keep state immutable by creating a copy
     const originalPins = this.props.pins.slice();
     // add ten empty divs here
-    const preEmptyDivs = Array(10 * newRounds).fill("empty div");
-    const postEmptyDivs = Array(10 * (this.state.maxRounds - newRounds)).fill("empty div");
-    const postEmpty = originalPins.slice(newStartIdx);
-    const pastZeroPins = originalPins.slice(0, newStartIdx);
+    const preEmptyDivs = Array(this.pinsPerPage * newRounds).fill("empty div");
+    const postEmptyDivs = Array(this.pinsPerPage * (this.state.maxRounds - newRounds)).fill("empty div");
+    const prePins = originalPins.slice(newStartIdx);
+    const postPins = originalPins.slice(0, newStartIdx);
 
-    const pins = preEmptyDivs.concat(postEmpty.concat(pastZeroPins.concat(postEmptyDivs)));
+    const pins = preEmptyDivs.concat(prePins, postPins, postEmptyDivs);
 
     this.setState({start_idx: newStartIdx, end_idx: newEndIdx, pins: pins, rounds: newRounds});
   }
 
+  downScrollIdxs() {
+    // NOTE: make this more readable
+    const start_idx = this.state.start_idx;
+
+    let newStartIdx = this.state.start_idx - this.pinsPerPage;
+    start_idx === 0 ? newStartIdx = this.totalPins : newStartIdx = start_idx - this.pinsPerPage;
+    let newEndIdx = (newStartIdx + this.pinsPerPage) % this.totalPins;
+    // avoid getting an invalid range of (40..0)
+    newEndIdx === 0 ? newEndIdx = this.totalPins : null;
+    // make this more readable
+
+    return [newStartIdx, newEndIdx];
+  }
+
   handleScrollCalculations(e) {
+    // adds elements from props to state with scroll
     const scrHeight = e.target.scrollHeight;
     const scrTop = e.target.scrollTop;
     const clientHeight = e.target.clientHeight;
-    const elementHeight = 300;
 
-    if (clientHeight + scrTop + 200 + (this.state.rounds * elementHeight * 10) >= scrHeight) {
+    // using 200 as a constant so that loading new elements starts before reaching bottom of the scroll bar
+
+    const topRounds = this.state.maxRounds - this.state.rounds;
+
+    if (clientHeight + scrTop + 200  + (topRounds * this.elementHeight * this.pinsPerPage) >= scrHeight) {
       let reOrderFlag;
       if (this.state.pins >= this.props.pins) {
         reOrderFlag = true
@@ -91,9 +123,9 @@ class List extends React.Component {
       else {
         reOrderFlag = false
       }
-      this.calculatePinIdx(reOrderFlag);
+      this.upScrollCalc(reOrderFlag);
     }
-    else if (this.state.rounds > 0 && scrTop - 200 <= (this.state.rounds * elementHeight * 10)) {
+    else if (this.state.rounds > 0 && scrTop - 200 <= (this.state.rounds * this.elementHeight * this.pinsPerPage)) {
       this.downScrollCalc();
     }
   }
@@ -104,6 +136,7 @@ class List extends React.Component {
   }
 
   pinsToRender(startIdx, endIdx) {
+    // adds elements from props to state with scroll
     const newPins = this.props.pins.slice(startIdx, endIdx)
 
     const pinsToRender = this.state.pins.concat(newPins);
